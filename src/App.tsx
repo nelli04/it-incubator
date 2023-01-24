0565816fdb257b00c5c33997a8c4387fd8e90092
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-24 19:24:45 +0300

    it04

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,18 @@ import Rating from './components/Rating/Rating';
 function App() {
   return (
     <div>
-    <Accordion/>
+    <PageTitle title = {'MyFriends'} />
+    <Accordion title = {'Меню'}/>
+    <PageTitle title = {'TITLE'} />
     <Rating value = {3}/>
     <Rating value = {4}/>
-    <PageTitle title = {'MyFriends'} />
-    <PageTitle title = {'TITLE'} />
     </div>
   );
 }
 
 function PageTitle(props: any) {
   console.log('PageTitle rendering')
-  return <h1>props.title</h1>
+  return <h1>{props.title}</h1>
 }
 
 export default App;
