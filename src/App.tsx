91cb88093b7ca6984f599e02aba5916391f566f3
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-24 17:30:47 +0300

    it04

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Accordion from './Accordion/Accordion';
+import Accordion from './components/Accordion/Accordion';
 
 function App() {
   return (
