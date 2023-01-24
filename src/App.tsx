14d1dea2f240c3ff352480ca6b8ea5fe9ad63677
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-24 16:09:48 +0300

    it03

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,53 @@ function App() {
   return (
     <div>
     <Star />
+    <Star />
+    <Star />
+    <Star />
+    <Star />
+    <Accordion/>
+    <Star />
+    <Star />
+    <Star />
+    <Star />
+    <Star />
     </div>
   );
 }
 
 function Star() {
+  console.log('Star rendering')
   return (
     <div>star</div>
   );
+}
+
+function Accordion() {
+  console.log('Accordion rendering')
+  return (
+    <div>
+  <AccordionTitle />
+  <AccordionBody />
+  </div>
+  )
+}
+
+function AccordionTitle() {
+  console.log('AccordionTitle rendering')
+  return (
+    <h3>Меню</h3>
+  )
+}
+
+function AccordionBody() {
+  console.log('AccordionBody rendering')
+  return (
+    <div>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    </div>
+  )
 }
 
 export default App;
