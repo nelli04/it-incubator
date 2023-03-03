f87246bf329010c0f590088752481c32a507b61c
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-03-03 12:27:13 +0300

    05Counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import React, {useState} from 'react';
 import './App.css';
-
 import {Counter} from "./components/Counter/Counter";
-
-
 
 function App() {
 
@@ -24,9 +21,11 @@ function App() {
   return (
       <div className='App'>
         <div className='container'>
-            <Counter click={click}
-                     buttonReset={buttonReset}
-                     buttonInc={buttonInc}/>
+
+                <Counter click={click}
+                         buttonReset={buttonReset}
+                         buttonInc={buttonInc}
+                />
         </div>
       </div>
   );
