a459d042db94a05823971dc57e1f1df54da79be0
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-03-03 11:59:38 +0300

    05Counter

--- src/components/Counter/Button.tsx
+++ src/components/Counter/Button.tsx
@@ -1,25 +1,24 @@
 import React, {useState} from 'react';
 
 
-
-
 type BossType = {
-    buttonInc: () => void
-    buttonReset: () => void
+    class: string
+    title: string
     click: number
+    onClick: () => void
 }
 
 export const Button = (p: BossType) => {
 
     return (
         <div>
-
-            <div className='btn'>
-                <button className={p.click >= 5 ? 'ic' : 'inc'} onClick={p.buttonInc}>inc</button>
-                <button className='set' onClick={()=> {}}>set</button>
-                <button className={p.click >= 1 ? 'reset' : 'true_false'} onClick={p.buttonReset}>reset</button>
-            </div>
+            <button onClick={p.onClick}>{p.title}</button>
         </div>
     );
 };
 
+/* <button className='set' onClick={()=> {}}>set</button>
+                <button className={p.click >= 1 ? 'reset' : 'true_false'} /*onClick={p.buttonReset}>reset</button>*/
+
+//buttonInc: () => void
+//buttonReset: () => void
