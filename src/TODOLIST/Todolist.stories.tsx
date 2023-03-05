99c24e441bc8bf47dfe978823ca6e21aad913d46
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-03-05 15:28:03 +0300

    05

---
+++ src/TODOLIST/Todolist.stories.tsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+/*
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+import {PropsType, Todolist} from "./Todolist";
+
+export default {
+    title: 'Example/Header',
+    component: Todolist,
+    parameters: {
+        // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+        layout: 'Todolist',
+    },
+} as ComponentMeta<typeof Todolist>;
+
+const Template: ComponentStory<typeof Todolist> = (args: PropsType) => {
+
+    //let tasksForTodolist = tasks;
+    return (
+        <Todolist {...args}/>
+    )
+}
+
+export const Accord = Template.bind({})
+Accord.args = {
+    title: '',
+    addTask: () => {
+    },
+    //tasks: tasksForTodolist,
+    changeTaskStatus: () => {
+    },
+    //filter: filter
+}
+
+export const LoggedOut = Template.bind({});
+LoggedOut.args = {};
+*/
