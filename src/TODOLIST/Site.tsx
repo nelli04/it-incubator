53b31bb4477309f7d17228744d47c8f1989d379f
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-03-05 18:43:53 +0300

    05Dop

---
+++ src/TODOLIST/Site.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import styles from './Site.module.css'
+import {Navigate, NavLink, Route, Routes} from "react-router-dom"
+import {Error404} from "./pages/Error404";
+import {Pages} from "./pages/Pages";
+import {dataState} from "./data/dataState";
+
+
+export const Site = () => {
+    return (
+        <div>
+            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.body}>
+                <div className={styles.nav}>
+                    {/*<div><NavLink to={'/page1'} className={({isActive}) => isActive ? styles.active : styles.navLink}>Page1</NavLink></div>*/}
+                    <div><NavLink to={'/page/0'} className={({isActive}) => isActive ? styles.active : styles.navLink}>Page1</NavLink></div>
+                    <div><NavLink to={'/page/1'} className={({isActive}) => isActive ? styles.active : styles.navLink}>Page2</NavLink></div>
+                    <div><NavLink to={'/page/2'} className={({isActive}) => isActive ? styles.active : styles.navLink}>Page3</NavLink></div>
+
+                </div>
+                <div className={styles.content}>
+                    <Routes>
+                        <Route path='/' element={<Navigate to={'/page/0'}/>}/>
+
+                        <Route path='/page/:id' element={<Pages pages={dataState.pages}/>}/>
+                       {/* <Route path='/page/:id' element={<PageTwo/>}/>
+                        <Route path='/page/:id' element={<PageThree/>}/>*/}
+
+                        <Route path='/*' element={<Error404/>}/>
+                    </Routes>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+//----------------------------------------------------------------------------------------------------------------------------------
+/*import React from 'react';
+import styles from './Site.module.css'
+import {Navigate, NavLink, Route, Routes} from "react-router-dom";
+import {PageOne} from "./pages/PageOne";
+import {PageTwo} from "./pages/PageTwo";
+import {PageThree} from "./pages/PageThree";
+import {Error404} from "./pages/Error404";
+
+
+export const Site = () => {
+    return (
+        <div>
+            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.body}>
+                <div className={styles.nav}>
+                    <div><NavLink to={'/page1'} className={({isActive}) => isActive ? styles.active : styles.navLink}>Page1</NavLink></div>
+                    <div><NavLink to={'/page2'} className={({isActive}) => isActive ? styles.active : styles.navLink}>Page2</NavLink></div>
+                    <div><NavLink to={'/page3'} className={({isActive}) => isActive ? styles.active : styles.navLink}>Page3</NavLink></div>
+
+                </div>
+                <div className={styles.content}>
+                    <Routes>
+                        <Route path='/' element={<Navigate to={'/page1'}/>}/>
+                        <Route path='/page1' element={<PageOne/>}/>
+                        <Route path='/page2' element={<PageTwo/>}/>
+                        <Route path='/page3' element={<PageThree/>}/>
+                        <Route path='/!*' element={<Error404/>}/>
+                    </Routes>
+
+                </div>
+            </div>
+        </div>
+    );
+};*/
+
