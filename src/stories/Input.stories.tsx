ff38d4aaa0c75ea66eb0ba25b4f46c6480e30ac1
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-03-11 11:06:09 +0300

    06Uncontrolled

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useRef, useState} from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import {Simulate} from "react-dom/test-utils";
 import input = Simulate.input;
@@ -15,8 +15,18 @@ export const UncontrolledInput = () => <input/>
 export const ControlledInput = () => <input value='It'/>
 export const TrackInput = () => {
     const [value, setValue] = useState('')
-    return <><input onChange={(e)=>{
+    const inputHandler = (e: ChangeEvent<HTMLInputElement>)=>{
         const actualValue = e.currentTarget.value
         setValue(actualValue)
-    }}/> - {value}</>
+    }
+    return <><input onChange={inputHandler}/> - {value}</>
+}
+export const GetTrackInputValue = () => {
+    const [value, setValue] = useState('')
+    const inputRef = useRef<HTMLInputElement>(null)
+    const inputHandler = () => {
+        const el = inputRef.current as HTMLInputElement
+        setValue(el.value)
+    }
+    return <><input ref={inputRef} id={'id'}/><button onClick={inputHandler}>save</button> actual value - {value}</>
 }
