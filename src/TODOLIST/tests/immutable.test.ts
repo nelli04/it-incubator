644d9c9eef71a6edb2a7fbd0ab0e75e86acf6ce4
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-03-12 15:26:43 +0300

    06

--- src/TODOLIST/tests/immutable.test.ts
+++ src/TODOLIST/tests/immutable.test.ts
@@ -1,4 +1,13 @@
-import {HasLaptopType, makeHairstyle, moveUser, upgradeUserLaptop, UserType} from "./immutable";
+import {
+    addNewBooks, companyUsers, CompanyUserType,
+    HasLaptopType,
+    makeHairstyle,
+    moveUser,
+    ReadBooksType, removeBook, SkillType, updateBook, updateSkill,
+    upgradeUserLaptop,
+    usersReadBooks,
+    UserType
+} from "./immutable";
 
 test ('reference type test', () => {
     let user: UserType = {
@@ -54,4 +63,164 @@ test ('upgrade laptop to macBook', () => {
     expect(user.address).toBe(userCopy.address)
     expect(user.laptop).not.toBe(userCopy.laptop)
     expect(userCopy.laptop.title).toBe('Macbook')
+})
+test ('users read books', () => {
+    let user: HasLaptopType & ReadBooksType = {
+        name: 'N',
+        hair: 32,
+        address: {
+            title: 'M',
+            house: 12
+        },
+        laptop: {
+            title: 'ZenBook'
+        },
+        books: [
+            'css', 'react', 'ts', 'refactor js'
+        ]
+    }
+
+    let userCopy = usersReadBooks(user, 100)
+
+    expect(user).not.toBe(userCopy)
+    expect(user.books).toBe(userCopy.books)
+    expect(user.address).not.toBe(userCopy.address)
+    expect(user.laptop).toBe(userCopy.laptop)
+    expect(userCopy.laptop.title).toBe('ZenBook')
+    expect(userCopy.address.house).toBe(100)
+})
+test ('users add new books', () => {
+    let user: HasLaptopType & ReadBooksType = {
+        name: 'N',
+        hair: 32,
+        address: {
+            title: 'M',
+            house: 12
+        },
+        laptop: {
+            title: 'ZenBook'
+        },
+        books: [
+            'css', 'react', 'ts', 'refactor js'
+        ]
+    }
+
+    let userCopy = addNewBooks(user, 'rest API')
+
+    expect(user).not.toBe(userCopy.books)
+    expect(user).not.toBe(userCopy)
+    expect(user.address).toBe(userCopy.address)
+    expect(user.laptop).toBe(userCopy.laptop)
+    expect(userCopy.laptop.title).toBe('ZenBook')
+    expect(user.address).toBe(userCopy.address)
+    expect(userCopy.books[4]).toBe('rest API')
+})
+test ('js to ts', () => {
+    let user: HasLaptopType & ReadBooksType = {
+        name: 'N',
+        hair: 32,
+        address: {
+            title: 'M',
+            house: 12
+        },
+        laptop: {
+            title: 'ZenBook'
+        },
+        books: [
+            'css', 'react', 'ts', 'refactor js'
+        ]
+    }
+
+    let userCopy = updateBook(user, 'js', 'ts')
+
+    expect(user).not.toBe(userCopy.books)
+    expect(user).not.toBe(userCopy)
+    expect(user.address).toBe(userCopy.address)
+    expect(user.laptop).toBe(userCopy.laptop)
+    expect(userCopy.laptop.title).toBe('ZenBook')
+    expect(user.address).toBe(userCopy.address)
+    expect(userCopy.books[2]).toBe('ts')
+})
+test ('skill to up', () => {
+    let user: HasLaptopType & ReadBooksType & SkillType = {
+        name: 'N',
+        hair: 32,
+        address: {
+            title: 'M',
+            house: 12
+        },
+        laptop: {
+            title: 'ZenBook'
+        },
+        books: [
+            'css', 'react', 'ts', 'refactor js'
+        ],
+        skill: [30]
+    }
+
+    let userCopy = updateSkill(user, 30, 100)
+
+    expect(user).not.toBe(userCopy.skill)
+    expect(user).not.toBe(userCopy)
+    expect(user.address).toBe(userCopy.address)
+    expect(user.laptop).toBe(userCopy.laptop)
+    expect(userCopy.laptop.title).toBe('ZenBook')
+    expect(user.address).toBe(userCopy.address)
+    //expect(userCopy.books[2]).toBe('ts')
+    expect(userCopy.skill[0]).toBe(100)
+})
+test ('remove js book', () => {
+    let user: HasLaptopType & ReadBooksType & SkillType = {
+        name: 'N',
+        hair: 32,
+        address: {
+            title: 'M',
+            house: 12
+        },
+        laptop: {
+            title: 'ZenBook'
+        },
+        books: [
+            'css', 'react', 'ts', 'refactor js'
+        ],
+        skill: [30]
+    }
+
+    let userCopy = removeBook(user, 'ts')
+
+    expect(user).not.toBe(userCopy.books)
+    expect(user).not.toBe(userCopy)
+    expect(user.address).toBe(userCopy.address)
+    expect(user.laptop).toBe(userCopy.laptop)
+    expect(userCopy.laptop.title).toBe('ZenBook')
+    expect(user.address).toBe(userCopy.address)
+    expect(userCopy.books[2]).toBe('refactor js')
+    //expect(userCopy.books.length).toBe(2)
+})
+test ('company user', () => {
+    let user: CompanyUserType & ReadBooksType & HasLaptopType = {
+        name: 'N',
+        hair: 32,
+        address: {
+            title: 'M',
+            house: 12
+        },
+        laptop: {
+            title: 'ZenBook'
+        },
+        books: [
+            'css', 'react', 'ts', 'refactor js'
+        ],
+        company: [
+            {id: 1, title: 'E'},
+            {id: 2, title: 'I'},
+        ]
+    }
+
+    let userCopy = companyUsers(user, 'Google', 1) as CompanyUserType & ReadBooksType
+
+    expect(user).not.toBe(userCopy)
+    expect(user.address).toBe(userCopy.address)
+    expect(user.company).not.toBe(userCopy.company)
+    expect(userCopy.company[0].title).toBe('Google')
 })
--- src/TODOLIST/tests/immutable.ts
+++ src/TODOLIST/tests/immutable.ts
@@ -1,27 +1,31 @@
-import {string} from "prop-types";
-
 export type UserType = {
     name: string
     hair: number
-    address: {title: string, house: number}
+    address: { title: string, house: number }
 }
-
 export type LaptopType = {
     title: string
 }
 export type HasLaptopType = UserType & {
     laptop: LaptopType
 }
-
-export function makeHairstyle (u: UserType, power: number) {
-    const copy = {
-        ...u,
-        hair:  u.hair / power
-    }
-    return copy
+export type ReadBooksType = UserType & {
+    books: string[]
+}
+export type SkillType = UserType & {
+    skill: number[]
+}
+export type CompanyUserType = {
+    company: Array<{title: string, id: number}>
 }
 
-export function moveUser (u: HasLaptopType, power: string) {
+export function makeHairstyle(u: UserType, power: number) {
+    return {
+        ...u,
+        hair: u.hair / power
+    }
+}
+export function moveUser(u: HasLaptopType, power: string) {
     return {
         ...u,
         address: {
@@ -32,14 +36,52 @@ export function moveUser (u: HasLaptopType, power: string) {
     }
     //copy.address = {...copy.address, title: power}
 }
-export function upgradeUserLaptop (u: HasLaptopType, laptop: string) {
+export function upgradeUserLaptop(u: HasLaptopType, laptop: string) {
     return {
         ...u,
         laptop: {
             ...u.laptop,
             title: laptop
         }
-        //laptop:  u.address.title === 'Msk'
     }
-    //copy.address = {...copy.address, title: power}
+}
+export function usersReadBooks(u: ReadBooksType & HasLaptopType, house: number) {
+    return {
+        ...u,
+        address: {
+            ...u.address,
+            house: house
+        }
+    }
+}
+export function addNewBooks(u: HasLaptopType & ReadBooksType, book: string) {
+    return {
+        ...u,
+        books: [...u.books, book]
+    }
+    //copy.books.concat(book)
+}
+export function updateBook(u: HasLaptopType & ReadBooksType, del: string, book: string) {
+    return {
+        ...u,
+        books: u.books.map(b => b === del ? book : b)
+    }
+}
+export function updateSkill(u: HasLaptopType & ReadBooksType & SkillType, del: number, book: number) {
+    return {
+        ...u,
+        skill: u.skill.map(s => s === del ? book : s)
+    }
+}
+export function removeBook(u: HasLaptopType & ReadBooksType, del: string) {
+    return {
+        ...u,
+        books: u.books.filter(f => f !== del)
+    }
+}
+export function companyUsers(u: CompanyUserType,  com: string, id: number) {
+    return {
+        ...u,
+        company: u.company.map(c => c.id === id ? {...c, title: com} : c)
+    }
 }
